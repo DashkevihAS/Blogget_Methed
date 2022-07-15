b2414db22e8401bcd94c141fc5e03dae4413eba0
DashkevichAS <antonsnov0604@gmail.com>
2022-07-15 14:30:12 +0300

    lesson3 task to check

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,19 +4,18 @@ import Layout from '../Layout';
 import Logo from './Logo';
 import Search from './Search';
 import Auth from './Auth';
-import Heading  from './Heading';
+import Heading from './Heading';
 
-export const Header = () => {
-    return (
-        <header className={style.header}>
-            <Layout>
-                <div className={style.gridContainer}>
-                    <Logo/>
-                    <Heading text='Главная'/>
-                    <Search/>
-                    <Auth auth={false} />   
-                </div>
-            </Layout>
-        </header>
-    )
-}
+export const Header = () =>
+  (
+    <header className={style.header}>
+      <Layout>
+        <div className={style.gridContainer}>
+          <Logo/>
+          <Heading text='Главная'/>
+          <Search/>
+          <Auth auth={false} />
+        </div>
+      </Layout>
+    </header>
+  );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import style from './Main.module.css';
 import Layout from '../Layout';
 
-export const Main = () => {
-    return (
-        <main className={style.main}>
-            <Layout>
+export const Main = () => (
+  <main className={style.main}>
+    <Layout>
 
-            </Layout>
-        </main>
-    );
-}
+    </Layout>
+  </main>
+);
