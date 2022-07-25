204be347f4997a7f6f6f549c94337121c57e7fdf
DashkevichAS <antonsnov0604@gmail.com>
2022-07-25 21:26:32 +0300

    add check time in Comments

--- src/components/Modal/Comments/Comments.jsx
+++ src/components/Modal/Comments/Comments.jsx
@@ -9,25 +9,29 @@ export const Comments = ({comments}) => {
   console.log(comments);
   return (
     <ul className={style.list}>
-      {comments.length > 0 ? comments.map(comment => (
-        <li key={comment.id} className={style.item}>
-          <Text As='h3'
-            className={style.author}
-            size={18}
-            tsize={22}
-          >
-            {comment.author}
-          </Text>
-          <Text As="p"
-            className={style.comment}
-            size={14}
-            tsize={18}
-          >
-            {comment.body}
-          </Text>
-          {comment.created && <Date date={comment.created} />}
-        </li>
-      )) : <Text As='p'
+      {comments.length > 0 ? comments.map(comment => {
+        if (comment.created) {
+          return (
+            <li key={comment.id} className={style.item}>
+              <Text As='h3'
+                className={style.author}
+                size={18}
+                tsize={22}
+              >
+                {comment.author}
+              </Text>
+              <Text As="p"
+                className={style.comment}
+                size={14}
+                tsize={18}
+              >
+                {comment.body}
+              </Text>
+              <Date date={comment.created} />
+            </li>
+          );
+        }
+      }) : <Text As='p'
         className={style.comment}
         size={14}
         tsize={18}
