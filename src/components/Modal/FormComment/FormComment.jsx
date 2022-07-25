4cffe83479d3ef3c622e4a6ab080d1ef774fc891
DashkevichAS <antonsnov0604@gmail.com>
2022-07-25 19:42:36 +0300

    additional task

--- src/components/Modal/FormComment/FormComment.jsx
+++ src/components/Modal/FormComment/FormComment.jsx
@@ -1,4 +1,4 @@
-import {useContext, useRef} from 'react';
+import {useContext, useRef, useState, useEffect} from 'react';
 import style from './FormComment.module.css';
 import {Text} from '../../../UI/Text';
 import {authContext} from '../../../context/authContext';
@@ -6,16 +6,39 @@ import {authContext} from '../../../context/authContext';
 export const FormComment = () => {
   const {auth} = useContext(authContext);
   const textareaRef = useRef(null);
+
+  const [isOpen, setIsOpen] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(textareaRef.current.value);
     textareaRef.current.value = '';
   };
+
+  const handlerClick = () => {
+    setIsOpen(true);
+  };
+
+  useEffect(() => {
+    if (isOpen) textareaRef.current.focus();
+  }, [isOpen]);
+
   return (
-    <form onSubmit={handleSubmit} className={style.form} >
-      <Text As='h3' size={14} tsize={18}>{auth.name}</Text>
-      <textarea ref={textareaRef} className={style.textarea} ></textarea>
-      <button className={style.btn} >Отправить</button>
-    </form>
+    <>
+      {isOpen ?
+      <form onSubmit={handleSubmit} className={style.form} >
+        <Text As='h3' size={14} tsize={18}>{auth.name}</Text>
+        <textarea
+          ref={textareaRef}
+          className={style.textarea}
+        >
+        </textarea>
+        <button className={style.btn} >Отправить</button>
+      </form> :
+      <button
+        className={style.btn}
+        onClick={handlerClick}
+      >Написать комментарий</button>}
+    </>
   );
 };
